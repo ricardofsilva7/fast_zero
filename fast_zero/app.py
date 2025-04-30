e281c9c879b0814fdc0c4440381c4012d512d298
ricardofsilva7 <ricardofsilva7@outlook.com>
2025-04-30 10:32:24 -0300

    📚 docs: Conteúdo Aula 3

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,29 +1,37 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
-
-from fast_zero.schemas import Message, UserSchema, UserPublic
-
 from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
 
+database = []
+
+
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return "{'message': 'Olá mundo!'}"
+    return {'message': 'Olá Mundo!'}
+
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    return user
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
 
-@app.get('/exercio-html', response_class=HTMLResponse)
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/exercicio-html', response_class=HTMLResponse)
 def exercicio_aula_02():
     return """
-        <html>
-            <head>
-                <title> Olá Mundo!</title>
-            </head>          
-            <body>
-                <h1>Olá Mundo!</h1>
-            </body>
-        </html>"""
+    <html>
+      <head>
+        <title>Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,43 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient  # type: ignore
+from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
 
-def test_read_root_deve_retornar_ok():
-    client = TestClient(app)  # Arrange (Organização)
+def test_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)
+    response = client.get('/')
 
-    response = client.get('/')  # Act (Ação)
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Olá Mundo!'}
 
-    assert response.status_code == HTTPStatus.OK  # Assert (Afirma)
 
-def test_exercicio_ola_mundo_html():
+def test_exercicio_ola_mundo_em_html():
     client = TestClient(app)
 
     response = client.get('/exercicio-html')
 
-    assert  response.status_code == HTTPStatus.OK
-    assert '<h1> Olá Mundo! </h1>' in response.txt
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={  # UserSchema
+            'username': 'testmyuser',
+            'password': 'password',
+            'email': 'test@test.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    # Validar UserPublic
+    assert response.json() == {
+        'username': 'testmyuser',
+        'email': 'test@test.com',
+        'id': 1,
+    }
